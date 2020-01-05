4438ab2e3ac3f7aa4d97c011a1b41a98a784e849
Bervyn Co <bervyn_co2010@yahoo.com>
2020-01-06 00:08:54 +0800

    Complete UI and redirection to login page feature for redeem

--- src/app/redeem/redeem.component.ts
+++ src/app/redeem/redeem.component.ts
@@ -1,4 +1,9 @@
-import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { DataService } from '../data.service';
+import { AlertController, ToastController, LoadingController } from '@ionic/angular';
+import { User } from '../interface/User';
 import {
 	BarcodeScannerOptions,
 	BarcodeScanner
@@ -10,18 +15,64 @@ import {
 	styleUrls: ['./redeem.component.css'],
 })
 export class RedeemComponent implements OnInit {
-	
+	points:Number;
+	isLoggedIn:boolean = false;
+	constructor(private router: Router, private authService: AuthService, private toastController: ToastController, private dataService: DataService) {
+		// this.loadStorage();
+		this.checkIfAuthenticated();
+	}
 	data:any = {
 		'userId': 41
 	}
 	encodedData:String = "";
+	
 	ngOnInit() {
 		this.generateQr();
 	}
+	
+	ionViewWillEnter() {
+		this.checkIfAuthenticated();
+	}
+	checkIfAuthenticated() {
+		this.authService.authState.subscribe(state => {
+			if(state == true){
+				this.getWalletDetails();
+			}
+			else {
+				this.createToast("You are not logged in. Please complete login.", 401, "bottom");
+				this.router.navigate(['me/login']);
+			}
+			this.isLoggedIn = state;
+		});
+	}
 
-  	ionViewDidLoad(){
+	getWalletDetails() {
+		this.dataService.getWalletDetails().subscribe(
+			(res:any)=>{
+				this.points = res.body[0].currentBalance;
+			},(err) => {
+			}
+		);
 	}
 	generateQr() {
 		this.encodedData = JSON.stringify(this.data);
+	}
+
+	async createToast(message, code, position){
+		let color = "";
+		if(code == 200)
+			color = "primary";
+		else
+			color = "danger";
+
+		const toast = await this.toastController.create({
+			color: color,
+	        duration: 3000,
+	        message: message,
+			showCloseButton: true,
+			animated: true,
+			position: position
+		});
+		toast.present();
 	}
 }
