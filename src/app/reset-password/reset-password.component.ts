a26faff48a382d6faad8f2e8d832809bf77207cc
Bervyn Co <bervyn_co2010@yahoo.com>
2020-01-06 00:09:54 +0800

    Complete reset password

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,  FormBuilder,  Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+import { AlertController, ToastController, LoadingController } from '@ionic/angular';
 
 @Component({
   	selector: 'app-reset-password',
@@ -6,9 +10,80 @@ import { Component, OnInit } from '@angular/core';
   	styleUrls: ['./reset-password.component.css'],
 })
 export class ResetPasswordComponent implements OnInit {
+	resetForm:FormGroup;
+	email:string;
+	password:string;
+	statusMessage:string;
+	spinnerLoader: any;
+	constructor(private formBuilder: FormBuilder, private alertController: AlertController, private toastController: ToastController, 
+			private loadingController: LoadingController, private dataService: DataService, private router: Router,
+	) { }
 
-  	constructor() { }
+  	ngOnInit() {
+		this.resetForm = this.formBuilder.group({
+			email: new FormControl('bervyn_co2010@yahoo.com', [
+				Validators.required
+			])
 
-  	ngOnInit() {}
+		});
+	}
+	showLoader() {
+		this.spinnerLoader = this.loadingController.create({
+			message: 'Please wait',
+			cssClass: '.custom-loading-class',
+			spinner: 'crescent',
+			
+		}).then((res) => {
+			console.log(res);
+			res.present();
+		});
+	}
+	 
+	hideLoader() {
+		setTimeout(() => {
+		  this.loadingController.dismiss();
+		}, 2000);
+	}
+	async createAlert(header, message){
+		const alert = await this.alertController.create({
+			header: header,
+			message: message,
+			buttons: ['OK']
+		});
+	
+		await alert.present();
+	}
+
+	async createToast(message, code, position){
+		let color = "";
+		if(code == 200)
+			color = "primary";
+		else
+			color = "danger";
+
+		const toast = await this.toastController.create({
+			color: color,
+	        duration: 2000,
+	        message: message,
+			showCloseButton: true,
+			animated: true,
+			position: position
+		});
+		toast.present();
+	}
+	reset() {
+		this.showLoader();
+		let details = this.resetForm.value;
+		
+		this.dataService.resetPassword(details).subscribe(
+			(res:any)=>{
+				this.createToast("Reset complete", 200, "bottom");
+				this.router.navigate(['/me']);
+				this.hideLoader();
+			},(err) => {
+				this.hideLoader();
+			}
+		);
+	}
 
 }
